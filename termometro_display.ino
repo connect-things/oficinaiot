2d96ca1f94f92f235028a031d6ec14d2c9855215
Francisco Calaça <chicocx@gmail.com>
2016-07-16 11:52:11 -0300

    Create termometro_display.ino

---
+++ termometro_display.ino
@@ -0,0 +1,79 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
+ 
+// Porta do pino de sinal do DS18B20
+// Ligue o DS18B20 na porta D3 (digital)
+#define ONE_WIRE_BUS 13
+ 
+// Define uma instancia do oneWire para comunicacao com o sensor
+OneWire oneWire(ONE_WIRE_BUS);
+
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+ 
+// Armazena as temperaturas minima e maxima
+float tempMin = 999;
+float tempMax = 0;
+ 
+DallasTemperature sensors(&oneWire);
+DeviceAddress sensor1;
+ 
+ 
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+  // Localiza e mostra enderecos dos sensores
+  Serial.println("Localizando sensores DS18B20...");
+  Serial.print("Foram encontrados ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" sensores.");
+  if (!sensors.getAddress(sensor1, 0)) 
+     Serial.println("Sensores nao encontrados !"); 
+  // Mostra o endereco do sensor encontrado no barramento
+  Serial.print("Endereco sensor: ");
+  mostra_endereco_sensor(sensor1);
+  Serial.println();
+  Serial.println();
+
+  lcd.begin(16, 2); //Inicia o LCD com dimensões 16x2(Colunas x Linhas)
+  lcd.setCursor(0, 0); //Posiciona o cursor na na primeira linha e primeira coluna
+}
+ 
+void mostra_endereco_sensor(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    // Adiciona zeros se necessário
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+ 
+ 
+void loop() {
+  // Le a informacao do sensor
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempC(sensor1);
+  // Atualiza temperaturas minima e maxima
+  if (tempC < tempMin)
+  {
+    tempMin = tempC;
+  }
+  if (tempC > tempMax)
+  {
+    tempMax = tempC;
+  }
+  // Mostra dados no serial monitor
+  Serial.print("Temp Atual C: ");
+  Serial.print(tempC);
+  Serial.print(" Min : ");
+  Serial.print(tempMin);
+  Serial.print(" Max : ");
+  Serial.println(tempMax);
+
+  lcd.setCursor(0, 0);
+  lcd.print(String(tempC));
+   
+  delay(1000);
+}
